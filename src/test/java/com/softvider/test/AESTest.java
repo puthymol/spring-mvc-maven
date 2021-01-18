7491a0d2afd8229d85e8e1e1722b7986d58ceade
Puthy <puthymol@gmail.com>
2021-01-18 09:38:05 +0700

    Add feature encrypt decrypt data using AES

--- src/test/java/com/softvider/test/AESTest.java
+++ src/test/java/com/softvider/test/AESTest.java
@@ -1,82 +1,51 @@
 package com.softvider.test;
 
+import com.softvider.utils.AESHexUtils;
+import com.softvider.utils.AESUtils;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import javax.crypto.Cipher;
-import javax.crypto.Mac;
-import javax.crypto.spec.SecretKeySpec;
-import java.nio.charset.StandardCharsets;
-import java.security.InvalidKeyException;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
-import java.util.Arrays;
-import java.util.Base64;
 
 public class AESTest {
-
     private static final Logger log = LoggerFactory.getLogger(AESTest.class);
 
     @Test
-    public void test() throws Exception {
-        String code = sign("data to hash", "wingsecretkey");
-        log.info(code);
-        code = encrypt("data to encrypt", "wingsecretkey");
-        log.info(code);
-        code = decrypt("+t8DA1s/aUle8DTp5Y4kRQ==", "wingsecretkey");
-        log.info(code);
+    public void testAES() throws Exception {
+        String plainText = "{\n" +
+                "  \"account_number\":\"0965270842\",\n" +
+                "  \"sender_id\":\"3311223482309755\",\n" +
+                "  \"lang\":\"en\",\n" +
+                "  \"service_type\":\"CB_CHECK_BALANCE\",\n" +
+                "  \"source\":\"facebook\",\n" +
+                "  \"recipient_id\": \"101427694631095\"\n" +
+                "}";
+        AESUtils aesUtils = new AESUtils();
+        String encrypted = aesUtils.encrypt(plainText);
+        log.info("encrypted <- {}", encrypted);
+        String decrypted = aesUtils.decrypt(encrypted);
+        log.info("decrypted <- {}", decrypted);
+        String javascript = "oEI27QOkvsnLlnTy0VpzpEm9Jz912qgDOilMoktm8DLJVcYM5Frc+czVMst+OwXww2cnVBQQj5CC4pcjml7gsFnWm2Sty867GKyeonT0Ie19wS/xCBFVgIVvx9zTEbUdJUnNnTV8zmn7OdDFW0AyHGhQh2tMYKQwsoo1+LDV5O9CCNewxdMHq7+ItylVrA5fepoQAWEEa/6nSP506rXIDZH9Xod9GqvnSa3AurxzmNqA1dMVHt5KzhQodq6FLdEt";
+        decrypted = aesUtils.decrypt(javascript);
+        log.info("decrypted from javascript <- {}", decrypted);
     }
 
-    public static String sign(String stringToHash, String key) throws InvalidKeyException, NoSuchAlgorithmException {
-        Mac sha256 = Mac.getInstance("HmacSHA256");
-        sha256.init(new SecretKeySpec(key.getBytes(StandardCharsets.UTF_8), "HmacSHA256"));
-        byte[] hash = sha256.doFinal(stringToHash.getBytes(StandardCharsets.UTF_8));
-        return byteToHex(hash);
-    }
-
-    public static String byteToHex(byte[] bytes) {
-        StringBuilder buffer = new StringBuilder();
-        for (byte aByte : bytes) {
-            buffer.append(Integer.toString((aByte & 0xff) + 0x100, 16).substring(1));
-        }
-        return buffer.toString();
-    }
-
-    public static String encrypt(String strToEncrypt, String key) throws Exception {
-        try {
-            SecretKeySpec secretKey = generateKey(key);
-            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5Padding");
-            cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-            return Base64.getEncoder().encodeToString(cipher.doFinal(strToEncrypt.getBytes(StandardCharsets.UTF_8)));
-        } catch (Exception e) {
-            log.info("Error while encrypting: " + e.toString());
-            throw new Exception(e.getMessage());
-        }
-    }
-
-    public static String decrypt(String strToDecrypt, String key)  throws Exception {
-        try {
-            SecretKeySpec secretKey = generateKey(key);
-            Cipher cipher = Cipher.getInstance("AES/ECB/PKCS5PADDING");
-            cipher.init(Cipher.DECRYPT_MODE, secretKey);
-            return new String(cipher.doFinal(Base64.getDecoder().decode(strToDecrypt)));
-        } catch (Exception e) {
-            log.info("Error while decrypting: " + e.toString());
-            throw new Exception(e.getMessage());
-        }
-    }
-
-    public static SecretKeySpec generateKey(String key) throws Exception {
-        try {
-            byte[] keyByte;
-            keyByte = key.getBytes(StandardCharsets.UTF_8);
-            MessageDigest sha = MessageDigest.getInstance("SHA-256");
-            keyByte = sha.digest(keyByte);
-            keyByte = Arrays.copyOf(keyByte, 16);
-            return new SecretKeySpec(keyByte, "AES");
-        }
-        catch (NoSuchAlgorithmException e) {
-            throw new Exception(e.getMessage());
-        }
+    @Test
+    public void testAESHex() throws Exception {
+        String plainText = "{\n" +
+                "  \"account_number\":\"0965270842\",\n" +
+                "  \"sender_id\":\"3311223482309755\",\n" +
+                "  \"lang\":\"en\",\n" +
+                "  \"service_type\":\"CB_CHECK_BALANCE\",\n" +
+                "  \"source\":\"facebook\",\n" +
+                "  \"recipient_id\": \"101427694631095\"\n" +
+                "}";
+        AESHexUtils aesHexUtils = new AESHexUtils();
+        String encrypted = aesHexUtils.encrypt(plainText);
+        log.info("encrypted <- {}", encrypted);
+        String decrypted = aesHexUtils.decrypt(encrypted);
+        log.info("decrypted <- {}", decrypted);
+        String javascript = "a2KZ5UZ2sTz99W5o7UbGhNIQumP8di2QfOw65FD6Ynxw3SMdLU/c9ti5YI3XOmLqvi7nkFgyDCgKlXFNWGpA6AXrfQPhaOfryteAEsnI1wvl0JxJyuCLwGcf3gUNkrlG89nGlCSeWQKbZzb83b84HqlPZlCT/ubTvLM7FP4CmgGEB7f4KWKGxBCYzXeTblhn9DOHLruu+9lxabr2uvlJ/r6y1vZ5YtW0CnjlH1BPbvLk0UBAdvYKz+PqOnKvg+Cj";
+        decrypted = aesHexUtils.decrypt(javascript);
+        log.info("decrypted from javascript <- {}", decrypted);
     }
 }
